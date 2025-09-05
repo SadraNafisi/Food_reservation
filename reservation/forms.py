61bcfb1bd295a7dec9fd4349915babdfd2c3c207
Sadra Nafisi <nafisisadra80@gmail.com>
2025-09-05 17:42:10 +0330

    add SubOrderForm in forms.py

---
+++ reservation/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import SubOrder
+class SubOrderForm(forms.ModelForm):
+    class Meta:
+        model = SubOrder
+        fields = ['amount']
+        widgets = {
+            "quantity":forms.NumberInput(attrs={"min":0})
+        }
